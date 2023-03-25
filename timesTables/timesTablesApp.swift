7701ee2bc64c6764bcaa5c89df021d6c28354fd8
mrbilby <baileymrb@gmail.com>
2023-03-25 18:03:44 +0000

    Initial Commit

---
+++ timesTables/timesTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  timesTablesApp.swift
+//  timesTables
+//
+//  Created by James Bailey on 25/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct timesTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
